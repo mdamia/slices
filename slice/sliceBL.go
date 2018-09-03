1073b0619aeea6fa25b11d9feeba12aa859924b2
Mohammed Damia <contacts@primetuts.com>
2018-09-02 23:26:59 -0700

    renambed slicebl foloder  slice

---
+++ slice/sliceBL.go
@@ -0,0 +1,9 @@
+package slice
+
+// AddPerson func
+func AddPerson(people []string, p1 string, p2 string, p3 string) []string {
+	people = append(people, p1)
+	people = append(people, p2)
+	people = append(people, p3)
+	return people
+}
